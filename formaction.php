ce285f86ac96f1bda8539cec4a2b5cb4467f01c1
Cdoherty95 <cdoherty@hawk.iit.edu>
2017-10-21 17:19:21 -0500

    cleaned up code. removed comments

--- formaction.php
+++ formaction.php
@@ -4,8 +4,6 @@
  * User: Chris
  * Date: 10/5/2017
  * Time: 12:33 PM
- *
- * require 'vendor/autoload.php';
  */
 
 ini_set('display_errors', 1);
@@ -41,7 +39,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     //move the file
     move_uploaded_file($tmp_name, $tmp_file_path);
-
 
     try{
         $s3Client = new S3Client([
@@ -129,57 +126,5 @@ printf("%d Row inserted.\n", $stmt->affected_rows);
 $stmt->close();
 $mysqli->close();
 
-
-
-
-/*local directory
-//$target_dir = "uploads/";
-//
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-//sets a variable for success
-$uploadOk = 1;
-
-//get files type (jpg, png) ect
-$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-
-// Check if image file is a actual image or fake image
-if (isset($_POST["submit"])) {
-    //boolean to see if file returns image size
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    //block to check if file is an image
-    if ($check !== false) {
-        //ensure file is in .jpg, .png, .jpeg, or .gif
-        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-            && $imageFileType != "gif"
-        ) {
-            //message if file is not
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-            $uploadOk = 0;
-        }
-        //message if file is good format and size
-        $uploadOk = 1;
-    } //else file is not in the proper format
-    else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-    //upload file to directory
-    //this will need to be replaced with call to s3 and my sqldb
-    move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-}
-*/
-
-
-/*
-//Need to put redirect to gallery page here with get methods
-if ($uploadOk == 1) {
-    $result = '<div class="alert alert-success">Image Has Been Uploaded</div>';
-} else {
-    $result = '<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-}
-//results not echoing
-
-echo $result;
-*/
 ?>
 
